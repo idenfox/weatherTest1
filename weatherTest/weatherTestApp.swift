0d6133a66c4349085b9f9730d8731a9f83ef1d17
Renzo Paul Chamorro <renzo607@gmail.com>
2021-04-17 16:14:51 -0500

    Initial Commit

---
+++ weatherTest/weatherTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherTestApp.swift
+//  weatherTest
+//
+//  Created by Renzo Paul Chamorro on 17/04/21.
+//
+
+import SwiftUI
+
+@main
+struct weatherTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
